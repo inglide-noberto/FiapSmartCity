84cf245d717adcfa7f3ddd8bc67edb82af6a89a4
Inglide Noberto <inglidenoberto961@gmail.com>
2022-12-01 12:59:03 -0300

    Adicionar arquivos de projeto.

---
+++ FiapSmartCity/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace FiapSmartCity.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string Caracteristicas { get; set; }
+        public double PrecoMedio { get; set; }
+        public string LogoTipo { get; set; }
+
+        public bool Ativo { get; set; }
+
+        // referencia para a classe TipoProduto
+        public TipoProduto  Tipo { get; set; }
+    }
+}
